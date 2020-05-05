7744354be212cbd3c69c88286b5d8941acbd2746
weiwei <wxjlmr@126.com>
2020-05-05 11:13:15 +0800

    添加源代码

---
+++ mockcpp/build/src/CMakeFiles/arg_related_headers.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../include/mockcpp/ArgumentsListDef.h"
+  "../../include/mockcpp/DelegatedMethodDef.h"
+  "../../include/mockcpp/MethodTypeTraitsDef.h"
+  "CMakeFiles/arg_related_headers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/arg_related_headers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mockcpp/build/src/CMakeFiles/vtbl_related_headers.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../include/mockcpp/DefaultMethodAddrGetterDef.h"
+  "../../include/mockcpp/DelegatedMethodGetByVptrDef.h"
+  "../../include/mockcpp/DelegatedMethodGetDef.h"
+  "../../include/mockcpp/DestructorAddrGetterDef.h"
+  "../../include/mockcpp/MethodIndiceCheckerDef.h"
+  "CMakeFiles/vtbl_related_headers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vtbl_related_headers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
